60d4b3cde870049353b6b7bbad1f5c345e574bd8
copaiement <cole.paiement@gmail.com>
2023-06-16 13:00:51 -0700

    Add caesarCipher and tests

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -3,9 +3,39 @@ function checkInputs(string, shift) {
   return false;
 }
 
+function shiftChar(char, shift) {
+  const charCode = char.charCodeAt(0);
+  // capitals
+  if (charCode >= 65 && charCode <= 90) {
+    let newCode = charCode + shift;
+    if (newCode < 65) {
+      newCode = 90 + ((newCode - 65) + 1);
+    } else if (newCode > 90) {
+      newCode = 65 + ((newCode - 90) - 1);
+    }
+    return String.fromCharCode(newCode);
+  }
+  // lowercase
+  if (charCode >= 97 && charCode <= 122) {
+    let newCode = charCode + shift;
+    if (newCode < 97) {
+      newCode = 122 + ((newCode - 97) + 1);
+    } else if (newCode > 122) {
+      newCode = 97 + ((newCode - 122) - 1);
+    }
+    return String.fromCharCode(newCode);
+  }
+
+  return char;
+}
+
 function caesarCypher(string, shift) {
   if (checkInputs(string, shift)) {
-
+    let newString = '';
+    for (let i = 0; i < string.length; i++) {
+      newString += shiftChar(string.charAt(i), shift);
+    }
+    return newString;
   }
   return 'Wrong input type. Required: string, number';
 }
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,2 +1,29 @@
 const caesarCipher = require('../scripts/caesarCipher');
 
+test('Shifts one letter', () => {
+  expect(caesarCipher('a', 5)).toBe('f');
+});
+
+test('Shifts one letter backwards', () => {
+  expect(caesarCipher('f', -5)).toBe('a');
+});
+
+test('Wraps forward', () => {
+  expect(caesarCipher('z', 5)).toBe('e');
+});
+
+test('Wraps backward', () => {
+  expect(caesarCipher('a', -5)).toBe('v');
+});
+
+test('Works with full string', () => {
+  expect(caesarCipher('This is a test', 10)).toBe('Drsc sc k docd');
+});
+
+test('Works with negative shift', () => {
+  expect(caesarCipher('Negative test', -5)).toBe('Izbvodqz ozno');
+});
+
+test('Works with punctuation', () => {
+  expect(caesarCipher('This. Is. A. TEST!', 3)).toBe('Wklv. Lv. D. WHVW!');
+});
